20732bc51de3f483bbd33d3b1c853967f18c3772
KudoShinya <KudoShinya@next-group.jp>
2015-08-04 13:10:34 +0900

    el-getをステージから削除

--- emacs.d/el-get/bundle-init/_home_kudos_.emacs.d_init-1_with-eval-after-load-feature-el.el
+++
@@ -1,9 +0,0 @@
-(require 'init-loader)
-(setq init-loader-show-log-after-init nil)
-(init-loader-load "~/.emacs.d/inits")
-(if
-    (not
-     (equal
-      (init-loader-error-log)
-      ""))
-    (init-loader-show-log))
