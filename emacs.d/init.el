f0ef6c52f51e9cb35320fa9eca605d6632cbd30f
KudoShinya <kudos@unit-nus.homes.co.jp>
2017-01-16 14:15:53 +0900

    add alias

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -47,19 +47,6 @@
  '(tool-bar ((t (:foreground "magenta" :box (:line-width 1 :style released-button))))))
 
 ;;
-;; Auto Complete
-;;
-(require 'auto-complete-config)
-(ac-config-default)
-(add-to-list 'ac-modes 'text-mode)         ;; text-modeでも自動的に有効にする
-(add-to-list 'ac-modes 'fundamental-mode)  ;; fundamental-mode
-(add-to-list 'ac-modes 'org-mode)
-(add-to-list 'ac-modes 'yatex-mode)
-(ac-set-trigger-key "TAB")
-(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
-(setq ac-use-fuzzy t)          ;; 曖昧マッチ
-
-;;
 ;;透過の設定
 ;;
 ;; (when window-system
@@ -74,35 +61,6 @@
 ;;(set-frame-parameter (selected-frame) 'alpha '(0.85))
 ;;(set-frame-parameter nil 'alpha 85)
 ;;(add-to-list 'default-frame-alist' (alpha . 85))
-
-;;
-;; el-get.el
-;;
-;;get el-get.el
-(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-;;(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-;;(el-get 'sync)
-
-;; (el-get 'sync
-;;   'color-moccur
-;;   'magit
-;; )
-
-;; load in ~/.emacs.d/inits
-(el-get-bundle tarao/with-eval-after-load-feature-el
-  (require 'init-loader)
-  (setq init-loader-show-log-after-init nil)
-  (init-loader-load "~/.emacs.d/inits")
-  (if (not (equal (init-loader-error-log) ""))
-      (init-loader-show-log))
-)
 
 ;;起動時にウィンドウを最大化する
 ;;(set-frame-parameter nil 'fullscreen 'maximized)
@@ -121,3 +79,145 @@
 
 ;;指定した行数に飛ぶ
 (global-set-key "\C-x\C-g" 'goto-line)
+
+;;C-2などでウインドウを分割したときに、ウインドウの大きさを変更する
+(defun window-resizer ()
+  "Control window size and position."
+  (interactive)
+  (let ((window-obj (selected-window))
+        (current-width (window-width))
+        (current-height (window-height))
+        (dx (if (= (nth 0 (window-edges)) 0) 1
+              -1))
+        (dy (if (= (nth 1 (window-edges)) 0) 1
+              -1))
+        action c)
+    (catch 'end-flag
+      (while t
+        (setq action
+              (read-key-sequence-vector (format "size[%dx%d]"
+                                                (window-width)
+                                                (window-height))))
+        (setq c (aref action 0))
+        (cond ((= c ?f)
+               (enlarge-window-horizontally dx))
+              ((= c ?b)
+               (shrink-window-horizontally dx))
+              ((= c ?n)
+               (enlarge-window dy))
+              ((= c ?p)
+               (shrink-window dy))
+              ;; otherwise
+              (t
+               (let ((last-command-char (aref action 0))
+                     (command (key-binding action)))
+                 (when command
+                   (call-interactively command)))
+               (message "Quit")
+               (throw 'end-flag t)))))))
+(global-set-key "\C-c\C-s" 'window-resizer)
+
+;; (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+;; (setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+;; (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+
+(defun mouse-scroll-down ()
+  (interactive)
+  (scroll-down 1))
+(defun mouse-scroll-up ()
+  (interactive)
+  (scroll-up 1))
+
+(global-set-key [wheel-up] 'mouse-scroll-down)
+(global-set-key [wheel-down] 'mouse-scroll-up)
+
+;; C-kでもクリップボードにコピーする
+(cond (window-system
+  (setq x-select-enable-clipboard t)
+  (setq x-select-enable-primary t)
+;;  (define-key global-map "\M-w" 'clipboard-kill-ring-save)
+;;  (define-key global-map "\C-w" 'clipboard-kill-region)
+;;  (define-key global-map "\C-y" 'clipboard-yank)
+))
+
+(add-to-list 'default-frame-alist '(font . "Ricty Diminished"))
+
+;; (set-face-attribute 'default nil
+;;                    :family "Ricty"
+;;                    :height 160)
+;; (set-fontset-font
+;;  nil 'japanese-jisx0208
+;;  (font-spec :family "Ricty"))
+
+;; (set-face-attribute 'default nil
+;;                     :family "Ricty Diminished Discord"
+;;                     :height 110)
+;; (set-fontset-font (frame-parameter nil 'font)
+;;                   'japanese-jisx0208
+;;                   (cons "Ricty Diminished Discord" "iso10646-1"))
+;; (set-fontset-font (frame-parameter nil 'font)
+;;                   'japanese-jisx0212
+;;                   (cons "Ricty Diminished Discord" "iso10646-1"))
+;; (set-fontset-font (frame-parameter nil 'font)
+;;                   'katakana-jisx0201
+;;                   (cons "Ricty Diminished Discord" "iso10646-1"))
+
+;;
+;; Auto Complete
+;;
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-modes 'text-mode)         ;; text-modeでも自動的に有効にする
+(add-to-list 'ac-modes 'fundamental-mode)  ;; fundamental-mode
+(add-to-list 'ac-modes 'org-mode)
+(add-to-list 'ac-modes 'yatex-mode)
+(ac-set-trigger-key "TAB")
+(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
+(setq ac-use-fuzzy t)          ;; 曖昧マッチ
+
+;;
+;; el-get.el
+;;
+;;get el-get.el
+(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+
+;; ;; load-path で ~/.emacs.d とか書かなくてよくなる
+;; (when load-file-name
+;;   (setq user-emacs-directory (file-name-directory load-file-name)))
+
+;; ;; el-get
+;; (add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
+;; (require 'el-get)
+;; ;; el-getでダウンロードしたパッケージは ~/.emacs.d/ に入るようにする
+;; (setq el-get-dir (locate-user-emacs-file "el-get"))
+
+;;(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+;;(el-get 'sync)
+
+;; (el-get 'sync
+;;   'color-moccur
+;;   'magit
+;; )
+
+;; load in ~/.emacs.d/inits
+(el-get-bundle tarao/with-eval-after-load-feature-el
+  (require 'init-loader)
+  (setq init-loader-show-log-after-init nil)
+  (init-loader-load "~/.emacs.d/inits")
+  (if (not (equal (init-loader-error-log) ""))
+      (init-loader-show-log))
+)
+
+(global-linum-mode t)
+
+(global-set-key (kbd "M-n") (lambda () (interactive) (scroll-up 1)))
+(global-set-key (kbd "M-p") (lambda () (interactive) (scroll-down 1)))
+
+(delete-selection-mode t)
--- zshrc
+++ zshrc
@@ -7,14 +7,9 @@ compinit
 #cd 忘れても通る
 setopt auto_cd
 
-# ↑を設定すると、 .. とだけ入力したら1つ上のディレクトリに移動できるので……
-# 2つ上、3つ上にも移動できるようにする
-alias ...='cd ../..'
-alias ....='cd ../../..'
-
 # "~hoge" が特定のパス名に展開されるようにする（ブックマークのようなもの）
 # 例： cd ~hoge と入力すると /long/path/to/hogehoge ディレクトリに移動
-hash -d php=/home/kudos/projects/homes/php_include
+hash -d homes=/home/kudos/projects/homes/
 
 # cd した先のディレクトリをディレクトリスタックに追加する
 # ディレクトリスタックとは今までに行ったディレクトリの履歴のこと
@@ -61,14 +56,33 @@ export FIGNORE=".svn:.git"
 #LS COLORS
 export LS_COLORS='no=00:fi=00:di=01;34;47:ln=01;36:pi=40;33:so=01;36:do=01;36:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.svgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.lzma=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.rar=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:';
 
+# ↑を設定すると、 .. とだけ入力したら1つ上のディレクトリに移動できるので……
+# 2つ上、3つ上にも移動できるようにする
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
 alias rm='rm -i'
-alias ls='ls -htF --color=tty --show-control-chars'
-alias ll='ls -l'
-alias la='ls -a'    
+alias ls='ls -al'
 alias rmb="rm ./*~"
 alias grep="grep --color"
 alias rspec="rspec -cfs"
- 
+
+alias e="emacs"
+alias e.="emacs ."
+
+alias g="git"
+alias gs="git status"
+alias gch="git checkout"
+alias gb="git branch"
+alias gl="git log --name-status"
+alias gsth="git stash"
+alias gdf="git diff"
+alias gfch="git fetch"
+alias gp="git pull"
+alias gpom="git pull origin master"
+alias gcm="git commit -m"
+alias ga='git add'
+
 #emacs
 alias em="emacsclient -nw -a ''"
 alias killemacs"emacsclient -e '(kill-emacs)'"
